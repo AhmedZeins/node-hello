9a77da1291fe4de00f742b44d64c6214ef9044e5
Ahmed Zein <ahuseein@banquemisr.com>
2025-05-06 22:20:35 +0300

    add fluentd log shipper for node-hello container

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,12 +7,44 @@ terraform {
   }
 }
 
-provider "docker" {
+provider "docker" {}
+
+
+
+variable "newrelic_license_key" {
+  description = "Your New Relic license key for log ingestion"
+  type        = string
 }
+
 
 resource "docker_image" "node_hello" {
   name = "zeinsss/node-hello:latest"
 }
+
+
+resource "docker_image" "fluentd" {
+  name         = "newrelic/newrelic-fluentd-docker:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "fluentd" {
+  name  = "newrelic-fluentd"
+  image = docker_image.fluentd.name
+
+  # Pass your license key to the Fluentd agent
+  env = [
+    "API_KEY=${var.newrelic_license_key}",
+    "BASE_URI=https://log-api.newrelic.com/log/v1"
+  ]
+
+  ports {
+    internal = 24224
+    external = 24224
+  }
+
+  restart = "always"
+}
+
 
 resource "docker_container" "node_hello" {
   name  = "node-hello-container"
@@ -22,4 +54,14 @@ resource "docker_container" "node_hello" {
     internal = 3000
     external = 3000
   }
+
+  # Send all stdout/stderr into Fluentd
+  log_driver = "fluentd"
+  log_opts = {
+    "fluentd-address" = "127.0.0.1:24224"
+    "tag"             = "node-hello-container"
+  }
+
+  # ensure the log shipper is up before the app
+  depends_on = [docker_container.fluentd]
 }
