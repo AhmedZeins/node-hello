0038b72f987798ffcef2e9dc22b16e1eb7627850
Ahmed Zein <ahuseein@banquemisr.com>
2025-05-06 19:06:02 +0300

    update

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+// eslint.config.js
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module",
+      ecmaVersion: "latest",
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        process: "readonly",
+        console: "readonly",
+      },
+    },
+    rules: {
+      indent: ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "single"],
+      semi: ["error", "always"],
+    },
+  },
+];
