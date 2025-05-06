eed96e15da1f23afdc8b1d6fe4d0edea8f7fc0e6
Ahmed Zein <ahuseein@banquemisr.com>
2025-05-06 21:55:31 +0300

    rename resource and add container configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,26 @@
 terraform {
- required_providers {
-   docker = {
-     source  = "kreuzwerker/docker"
-     version = "3.0.2"
-   }
- }
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
 }
-resource "docker_image" "node-hello" {
- name = "zeinsss/node-hello:latest"
+
+
+resource "docker_image" "node_hello" {
+  name = "zeinsss/node-hello:latest"
 }
+
+resource "docker_container" "node_hello" {
+  name  = "node-hello-container"
+  image = docker_image.node_hello.name
+
+  ports {
+    internal = 3000
+    external = 3000
+  }
+}
+
+
+
